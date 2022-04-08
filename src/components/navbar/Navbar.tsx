c8d7ed0fdb03a9e299b878fe3746fd2195387673
mahdi <mahdi.hdd2001@gmail.com>
2022-04-08 18:55:23 +0430

    create navbar footer

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import "./assets/navbar.scss";
 import User from "./components/User";
 import Nav from "./components/Nav";
 import useWindowDimensions from "./../../hooks/useWindowDimensions";
-import { BiMenu, BiLogOut } from "react-icons/bi";
-import { BsFillQuestionSquareFill, BsSearch } from "react-icons/bs";
+import { BiMenu, BiSearch } from "react-icons/bi";
+import { BsFillQuestionSquareFill } from "react-icons/bs";
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
   const { width } = useWindowDimensions();
@@ -20,38 +20,18 @@ const Navbar = () => {
             <BiMenu size={"2rem"} color="#fff" />
           </div>
         )}
-        {/* <Logo />
-
-        {width >= 769 ? (
-          <>
-            <Nav />
-            <User />
-          </>
-        ) : (
-          <>
-            <div className="about">
-              <BsFillQuestionSquareFill size={"1.5rem"} color="#fff" />
-            </div>
-            <div className="menu" onClick={() => setMenu(!menu)}>
-              <BiMenu size={"2rem"} color="#fff" />
-            </div>
-          </>
-        )} */}
       </div>
-      {/* {width < 768 && (
-        <div className="footer-navbar">
-          <div className="search">
-            <input type="text" placeholder="جستوجو" />
-            <span className="ico">
-              <BsSearch color="#fff" />
-            </span>
-          </div>
-          <div className="d-flex j-c-c a-i-c">
-            <span className="mx-4">ورود |</span>
-            <BiLogOut color="#fff" size={"1.5rem"} />
-          </div>
+      <div className="footer-navbar">
+        <div className="search">
+          <span className="ico">
+            <BiSearch color="#fff" />
+          </span>
+          <input type="text" placeholder="جستوجو" />
         </div>
-      )} */}
+        <div className="question">
+          <BsFillQuestionSquareFill color="#fff" size={'1.5rem'} />
+        </div>
+      </div>
     </div>
   );
 };
